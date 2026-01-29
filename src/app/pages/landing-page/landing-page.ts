f9ec17cafde70b0af220e771a66bd71886b56922
Kavyasrinivas11 <kavyasrinivas1102@gmail.com>
2026-01-29 16:32:28 +0530

    EMI Calculator

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class LandingPage {
 
+  constructor(private route:Router){}
+
+  button(){
+    this.route.navigateByUrl('/login')
+  }
 }
